a1891b02f79a442e1914bf4b3d9f44119dfe1565
Aikarubi <nerupe@floridauniversitaria.es>
2024-10-16 17:24:42 +0200

    funcion para obtener todos los coches

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -6,3 +6,8 @@ const fs = require('fs');
 //Leer los datos del archivo JSON
 const carsFilePath = path.join(__dirname, '../../data/cars.json');
 let carsData = JSON.parse(fs.readFileSync(carsFilePath, 'utf-8'));
+
+//Funcion para obtener todos los coches
+const getAllCars = (req, res) => {
+    res.json(carsData);
+}
