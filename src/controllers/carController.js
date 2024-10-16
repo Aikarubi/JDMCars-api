104681256afd397552c7ca4180b0721aef69ec77
Aikarubi <nerupe@floridauniversitaria.es>
2024-10-16 17:26:49 +0200

    funcion para obtener el coche por id y exportacion de fnciones

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -11,3 +11,20 @@ let carsData = JSON.parse(fs.readFileSync(carsFilePath, 'utf-8'));
 const getAllCars = (req, res) => {
     res.json(carsData);
 }
+
+//Funcion para obtener un coche por su id
+const getCarById = (req, res) => {
+    const carId = req.params.id;
+    const car = carsData.find(car => car.id === carId);
+    
+    if (car) {
+        res.json(car);
+    } else {
+        res.status(404).send('Coche no encontrado');
+    }
+};
+
+module.exports = {
+    getAllCars,
+    getCarById
+};
