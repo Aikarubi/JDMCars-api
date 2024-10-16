142d4710806009e57634f8b879dd959f975bd3b1
Aikarubi <nerupe@floridauniversitaria.es>
2024-10-16 17:19:33 +0200

    añadiendo un par de rutas simples

---
+++ src/routes/carRoutes.js
@@ -0,0 +1,11 @@
+//Rutas para hacer peticiones a la base de datos
+
+const express = require('express');
+const router = express.Router();
+const carController = require('../controllers/carController');
+
+//Rutas
+router.get('/cars', carController.getAllCars); //Obtener todos los coches
+router.get('/cars/:id', carController.getCarById); //Obtener un coche por su id
+
+module.exports = router
