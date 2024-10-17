707ab3a119469221e07e8cb9340d9af749f8c34e
Aikarubi <nerupe@floridauniversitaria.es>
2024-10-17 11:06:07 +0200

    añadiendo nueva ruta

--- src/routes/carRoutes.js
+++ src/routes/carRoutes.js
@@ -7,5 +7,6 @@ const carController = require('../controllers/carController');
 //Rutas
 router.get('/cars', carController.getAllCars); //Obtener todos los coches
 router.get('/cars/:id', carController.getCarById); //Obtener un coche por su id
+router.get('/cars/search', carController.searchCars); //Buscar coches por marca
 
 module.exports = router
