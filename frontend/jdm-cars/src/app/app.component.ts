403f55072e1470bc672864f4b8965eb68124895c
Aikarubi <nerupe@floridauniversitaria.es>
2025-02-21 19:51:36 +0100

    creando componentes

--- frontend/jdm-cars/src/app/app.component.ts
+++ frontend/jdm-cars/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { CopyBarComponent } from './copy-bar/copy-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavComponent, FooterComponent, CopyBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
