86730718b3904e3bd5fcdb00085d5f20ecb0a2f5
Aikarubi <nerupe@floridauniversitaria.es>
2025-02-22 12:25:28 +0100

    footer y copy-bar completo con estructura y CSS, además creación de rutas

--- frontend/jdm-cars/src/app/app.component.ts
+++ frontend/jdm-cars/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { CopyBarComponent } from './copy-bar/copy-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavComponent, FooterComponent, CopyBarComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CopyBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
